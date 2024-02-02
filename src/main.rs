a539ece3fca8b188122ee3e134d4d457f8000238
Frank Uhlig <uhlig.frank@gmail.com>
2024-02-02 22:57:07 +0100

    add: locating rules section

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,31 @@ fn main() {
         process::exit(1);
     });
 
-    let content = fs::read_to_string(source);
+    let content = fs::read_to_string(source).unwrap_or_else(|err| {
+        eprintln!("Error reading from file {}", err);
+        process::exit(1);
+    });
+
+    let rules_start = match content.find("%%") {
+        Some(index) => index,
+        None => {
+            eprintln!("Could not find start of grammar rules");
+            process::exit(1);
+        }
+    };
+
+    let rules_end = match content[rules_start..].find("%%") {
+        Some(index) => index,
+        None => {
+            eprintln!("Could not find end of grammar rules");
+            process::exit(1);
+        }
+    };
+
+    let content = &content[rules_start + 2..rules_end];
+    let content = remove_semantic_actions(content.trim());
+}
+
+fn remove_semantic_actions(rule: &str) -> &str {
+    rule
 }
